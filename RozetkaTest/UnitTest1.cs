3a9c6a775086fc0f1d3a4dac79843fcd4d929634
Marmadyk <95248327+Marmadyk@users.noreply.github.com>
2021-12-09 19:50:03 +0200

    waiter update

--- RozetkaTest/UnitTest1.cs
+++ RozetkaTest/UnitTest1.cs
@@ -37,6 +37,7 @@ namespace RozetkaTest
             driver = new OpenQA.Selenium.Chrome.ChromeDriver();
             driver.Navigate().GoToUrl("https://rozetka.com.ua/ua/");
             driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
         }
 
         [Test]
@@ -45,74 +46,58 @@ namespace RozetkaTest
             var phoneCategorySelect = driver.FindElement(phoneCategory);
             phoneCategorySelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var smartPhoneSelect = driver.FindElement(smartPhone);
             smartPhoneSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var appleProducerSelect = driver.FindElement(appleProducer);
             appleProducerSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var sortPhonesSelect = driver.FindElement(sortFromExpensiveToCheap);
             sortPhonesSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var addPhoneToCartButtonClick= driver.FindElement(addToCartButton);
             addPhoneToCartButtonClick.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var categoryDropDownButtonClick = driver.FindElement(categoryDropDownButton);
             categoryDropDownButtonClick.Click();
             var tvCategorySelect = driver.FindElement(tvCategory);
             tvCategorySelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var tvCategoryClick = driver.FindElement(tvCategoryButton);
             tvCategoryClick.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var samsungProducerSelect = driver.FindElement(samsungProducer);
             samsungProducerSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var sortTvSelect = driver.FindElement(sortFromExpensiveToCheap);
             sortTvSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var addTvToCartButtonClick = driver.FindElement(addToCartButton);
             addTvToCartButtonClick.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var categoryButtonClick = driver.FindElement(categoryDropDownButton);
             categoryButtonClick.Click();
             var goodsForGamersCategorySelect = driver.FindElement(goodsForGamersCategory);
             goodsForGamersCategorySelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var gameConsolesCategorySelect = driver.FindElement(gameConsolesCategory);
             gameConsolesCategorySelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var microsoftProducerSelect = driver.FindElement(microsoftProducer);
             microsoftProducerSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var sortGameConsolesSelect = driver.FindElement(sortFromExpensiveToCheap);
             sortGameConsolesSelect.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var addConsoleToCartButtonClick = driver.FindElement(addToCartButton);
             addConsoleToCartButtonClick.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             var actualQttOfItemsInCart = driver.FindElement(counterOfItemsInCart).Text;
             Assert.AreEqual(expectedCountOfItemsInCart, actualQttOfItemsInCart, "not enough items");
 
             var CartbuttonClick = driver.FindElement(cartButton);
             CartbuttonClick.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             var amountInCart = driver.FindElement(totalAmountInCart).Text;
             int.TryParse(string.Join("", amountInCart.Where(c => char.IsDigit(c))), out int sumInCart);
             bool compareAmountInCart = (sumInCart/27) < 5000;
